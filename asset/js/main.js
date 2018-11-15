92af5f40469f406a712f39a71228dc548c9949bf
snowstop0327 <hst-790327@hotmail.com>
2018-11-15 13:42:30 +0800

    add asset source

---
+++ asset/js/main.js
@@ -0,0 +1,89 @@
+// var bodyClass = document.body.classList,
+//     lastScrollY = 0;
+// window.addEventListener('scroll', function(){
+//   var st = this.scrollY;
+//   // 判斷是向上捲動，而且捲軸超過 200px
+//   if( st < lastScrollY) {
+//     bodyClass.remove('fixed-hide');
+//   }else{
+//     bodyClass.add('fixed-hide');
+//   }
+//   lastScrollY = st;
+// });
+
+
+// navbar-toggle-button
+
+// $('.navbar-toggler').click(function (event) {
+//   /* Act on the event */
+//   $(this).toggleClass('icon-android-close');
+//   // $('.header-fixed').toggleClass('is-sticky');
+//   $('.navbar-end').toggleClass('open');
+// });
+
+
+// 平滑捲動毛點
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top -80
+    }, 1200, "swing");
+});
+
+// 滾動效果
+$(document).ready(function() {
+	$('.scroll-target').click(function() {
+		event.preventDefault(); // 增加效能
+		var target = $(this).attr('href');
+		var targetPos = $(target).offset().top -80;
+
+		$('html, body').animate({scrollTop: targetPos}, 1000);
+
+	});
+	var showSkill = false; // 為了讓滾動條只顯示一次，否則他會因為一直滾動的狀態重置
+
+
+	// 視窗滾動
+	$(window).scroll(function(){
+		var scrollPos = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		// console.log(scrollPos, windowHeight);
+		// console.log(scrollPos);
+		// 
+		// 滾動nav錨點對應的區域
+		$('.scroll-target').each(function() {
+			var target = $(this).attr('href');
+			var targetPos = $(target).offset().top -90;
+			var targetHeight = $(target).outerHeight();// 可包含了padding的高度
+			// console.log(target, targetPos, targetHeight);
+			if (targetPos  <= scrollPos && (targetPos + targetHeight) > scrollPos) {
+				// console.log(target);
+				$(this).addClass('is-active');
+			} else {
+				$(this).removeClass('is-active');
+			}
+		});
+
+		// progress bar
+		
+		var skillTop = $('#skill').position().top;
+		// console.log('skillTop', skillTop);
+		if (skillTop <= scrollPos + (windowHeight / 1.5)&& !showSkill) {
+			// showSkill = true;
+			$('#skill .progress > span').each(function() {
+				var thisValue = $(this).data('progress');
+				// console.log('thisValue', thisValue);
+				$(this).css('width', thisValue + '%');
+			});
+		}
+
+		// header show fixed
+		if (scrollPos >= windowHeight +2) {
+			$('body').removeClass('fixed-hide');
+		}
+		else {
+			$('body').addClass('fixed-hide');
+		}
+	})
+});
