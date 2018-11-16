22291b29608806720f26922d0a35f76041094d50
snowstop0327 <hst-790327@hotmail.com>
2018-11-16 15:52:17 +0800

    timeline-animation complete

--- asset/js/main.js
+++ asset/js/main.js
@@ -91,3 +91,31 @@ $(document).ready(function() {
 		}
 	})
 });
+
+// timeline-animation
+jQuery(document).ready(function($){
+	var timelineBlocks = $('.timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).addClass('is-visibility-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).hasClass('is-visibility-hidden') ) && $(this).removeClass('is-visibility-hidden').addClass('is-move-in');
+		});
+	}
+});
